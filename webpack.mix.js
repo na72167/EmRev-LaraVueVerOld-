fd399d1bf64af4b67d545cb87845bea417673d95
yh72167 <yh.19972372@gmail.com>
2021-03-30 23:44:56 +0900

    テストの設定など

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 const mix = require('laravel-mix')
 
     mix.browserSync('vuesplash.test')
+    .sass('resources/sass/app.scss', 'public/css');
     .js('resources/js/app.js', 'public/js')
     .version()
